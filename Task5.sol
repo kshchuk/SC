454d83d430f86b1765ed296dca6c7abc0029d41c
kshchuk <kshchuk@gmail.com>
2023-05-09 00:21:09 +0300

    Task 5 implemented

--- Task5.sol
+++ Task5.sol
@@ -28,6 +28,24 @@ contract UintStorage {
 
 contract StrangeCalculator is UintStorage {
     function setNewValues(uint256 first, Point calldata point) external override {
-        // TODO
+        uint256 key = 12;
+
+        assembly {
+            sstore(0x0, first) // changes the "one" 
+
+            mstore(0x0, key) // stores the key in memorys
+            mstore(0x20, 1) // ensures that memory position 0x20 is aligned to a 32-byte boundary by setting its value to 1
+            let slot := keccak256(0x0, 0x40) // gets Point's slot
+
+            sstore(slot, calldataload(0x24)) // stores the "x" value 
+            sstore(add(slot, 1), calldataload(0x44)) // stores the "y" value in the next storage slot 
+        }
+    }
+}
+
+
+contract Tester {
+function set(StrangeCalculator strangeCalculator_) external {
+        strangeCalculator_.setNewValues(734673, Point(3525, 245425));
     }
 }
